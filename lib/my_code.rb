b12d574fee33b14ade18299467adfaf3910e83dc
Miwha Geschwind <miwhag@gmail.com>
2020-01-01 01:22:47 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -51,17 +51,6 @@ end
 
 
 
-# def reduce_to_total(source_array,starting_point) 
-# index = 0 
-
-# while index < source_array.length
-# source_array[index]++
-# index += 1 
-# end
- 
-#   return result 
-#   end
- 
 
 
 def reduce_to_total(source_array, starting_point = 0) 
